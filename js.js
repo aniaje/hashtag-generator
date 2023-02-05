665f8e6d75c5e5bcc00ba1a109397bb97cb2c457
Ana <jezowskanna@gmail.com>
2023-02-05 09:56:35 +0100

    copy to clipboard added

--- js.js
+++ js.js
@@ -1,9 +1,9 @@
 const input = document.querySelector("#text-input");
-const output = document.querySelector("p");
+const hashtag = document.getElementById("hashtag");
 const button = document.getElementById("b");
 const copyContentBtn = document.getElementById("copyContentBtn");
 button.addEventListener("click", generateHashtag);
-copyContentBtn.addEventListener("click", copyContent);
+copyContentBtn.addEventListener("click", copyToClipboard);
 
 function generateHashtag() {
   const str = input.value;
@@ -13,14 +13,13 @@ function generateHashtag() {
     return letter != " " ? hash + letter : hash;
   }, "#");
   console.log(result);
-  output.innerHTML = result;
-  return result.length == 1 || result.length > 140 ? false : result;
+  hashtag.innerHTML = result;
+  result.length == 1 || result.length > 140 ? false : result;
 }
 
-const copyContent = async () => {
-  try {
-    await navigator.clipboard.writeText(result);
-  } catch (err) {
-    console.error("Failed to copy: ", err);
-  }
-};
+function copyToClipboard() {
+  let copyText = hashtag.innerHTML;
+  navigator.clipboard.writeText(copyText).then(() => {
+    copyContentBtn.innerHTML = "&#10003";
+  });
+}
